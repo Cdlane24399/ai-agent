66197d891b28b3c45ed5e2924ab64660ca55b501
chris <cd.lane@icloud.com>
2025-06-18 13:26:14 -0400

    Initial Commit

---
+++ Ai-Agent/Ai_AgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ai_AgentApp.swift
+//  Ai-Agent
+//
+//  Created by Chris Lane on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Ai_AgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
